95f615e81428448b566cc2dba11bbb4777dbda70
JorgeIFCE <105126992+JorgeIFCE@users.noreply.github.com>
2023-05-18 18:05:35 -0300

    Script de movimento

---
+++ Wasteland World/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rbPlayer;
+    [SerializeField] float speed = 5f;
+
+    [SerializeField] float jumpForce = 15f;
+    [SerializeField] bool isJump;
+    [SerializeField] bool inFloor = true;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+
+    Animator animPlayer;
+
+
+    private void Awake() 
+    {
+        animPlayer = GetComponent<Animator>();
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inFloor = Physics2D.Linecast(transform.position,groundCheck.position, groundLayer);
+        Debug.DrawLine(transform.position,groundCheck.position, Color.blue);
+
+        if (Input.GetButtonDown("Jump") && inFloor)
+            isJump = true;
+        else if (Input.GetButtonUp("Jump") && rbPlayer.velocity.y > 0)
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, rbPlayer.velocity.y * 0.5f);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        JumpPlayer();
+    }                                        
+
+    void Move()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        rbPlayer.velocity = new Vector2(xMove * speed, rbPlayer.velocity.y);
+
+        animPlayer.SetFloat("Speed", Mathf.Abs(xMove));
+
+        if (xMove > 0)
+        {
+            transform.eulerAngles = new Vector2(0,0);
+        }
+        else if (xMove < 0)
+        {
+            transform.eulerAngles = new Vector2(0,180);
+        }
+    }
+
+    void JumpPlayer()
+    {
+        if (isJump)
+        {
+            rbPlayer.velocity = Vector2.up * jumpForce;
+            isJump = false;
+        }
+    }
+   
+}
